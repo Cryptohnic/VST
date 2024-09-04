a8574436913e2c6fbe7975318c2cbf847902b876
Brandon A <github.abdominal373@passmail.net>
2024-09-04 12:47:21 -0400

    Create Settings Chain

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -104,6 +104,14 @@ void VSTAudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlock)
     
     leftChain.prepare(spec);
     rightChain.prepare(spec);
+    
+    auto chainSettings = getChainSettings(apvts);
+    auto peakCoefficients = juce::dsp::IIR::Coefficients<float>::makePeakFilter(
+                                                                sampleRate,
+                                                                chainSettings.peakFreq,
+                                                                chainSettings.peakQuality,
+                                                                juce::Decibels::decibelsToGain(chainSettings.peakGainInDecibels));
+    
 }
 
 void VSTAudioProcessor::releaseResources()
@@ -189,6 +197,21 @@ void VSTAudioProcessor::setStateInformation (const void* data, int sizeInBytes)
 {
     // You should use this method to restore your parameters from this memory block,
     // whose contents will have been created by the getStateInformation() call.
+}
+
+ChainSettings getChainSettings(juce::AudioProcessorValueTreeState &apvts){
+    ChainSettings settings;
+    
+    // get real time values based on our ranges
+    settings.lowCutFreq=apvts.getRawParameterValue("LowCut Freq")->load();
+    settings.highCutFreq=apvts.getRawParameterValue("HighCut Freq")->load();
+    settings.peakFreq=apvts.getRawParameterValue("Peak Freq")->load();
+    settings.peakGainInDecibels=apvts.getRawParameterValue("Peak Gain")->load();
+    settings.peakQuality=apvts.getRawParameterValue("Peak Quality")->load();
+    settings.lowCutSlope=apvts.getRawParameterValue("LowCut Slope")->load();
+    settings.highCutSlope=apvts.getRawParameterValue("HighCut Slope")->load();
+    
+    return settings;
 }
 
 juce::AudioProcessorValueTreeState::ParameterLayout VSTAudioProcessor::createParameterLayout(){
